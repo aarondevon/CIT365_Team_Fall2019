815529b11d23ebd3c000faae2dbbe6a7381f57e9
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-10-19 09:32:05 -0500

    Restore files to original state

--- MegaDesk-Bountiful/ViewAllQuotes.Designer.cs
+++ MegaDesk-Bountiful/ViewAllQuotes.Designer.cs
@@ -28,70 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelQuotes = new System.Windows.Forms.Label();
-            this.labelRushValue = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewAllQuotes));
+            this.MainMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // labelQuotes
+            // MainMenu
             // 
-            this.labelQuotes.AutoSize = true;
-            this.labelQuotes.Location = new System.Drawing.Point(100, 95);
-            this.labelQuotes.Name = "labelQuotes";
-            this.labelQuotes.Size = new System.Drawing.Size(46, 17);
-            this.labelQuotes.TabIndex = 0;
-            this.labelQuotes.Text = "label1";
-            // 
-            // labelRushValue
-            // 
-            this.labelRushValue.AutoSize = true;
-            this.labelRushValue.Location = new System.Drawing.Point(218, 95);
-            this.labelRushValue.Name = "labelRushValue";
-            this.labelRushValue.Size = new System.Drawing.Size(46, 17);
-            this.labelRushValue.TabIndex = 1;
-            this.labelRushValue.Text = "label2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(103, 264);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(238, 264);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.MainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(156)))), ((int)(((byte)(238)))));
+            this.MainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MainMenu.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenu.ForeColor = System.Drawing.Color.White;
+            this.MainMenu.Location = new System.Drawing.Point(12, 12);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(165, 60);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "&Main Menu";
+            this.MainMenu.UseVisualStyleBackColor = false;
+            this.MainMenu.Click += new System.EventHandler(this.MainMenu_Click);
             // 
             // ViewAllQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.labelRushValue);
-            this.Controls.Add(this.labelQuotes);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.MainMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ViewAllQuotes";
             this.Text = "ViewAllQuotes";
-            this.Load += new System.EventHandler(this.ViewAllQuotes_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label labelQuotes;
-        private System.Windows.Forms.Label labelRushValue;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button MainMenu;
     }
 }
--- MegaDesk-Bountiful/ViewAllQuotes.cs
+++ MegaDesk-Bountiful/ViewAllQuotes.cs
@@ -6,50 +6,22 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 using System.Windows.Forms;
 
 namespace MegaDesk_Sawyer
 {
     public partial class ViewAllQuotes : Form
     {
-        private DeskQuote deskQuote;
-        private MainMenu mainMenu;
-
         public ViewAllQuotes()
-
         {
             InitializeComponent();
         }
 
-        public ViewAllQuotes(DeskQuote deskQuote, MainMenu mainMenu)
-
+        private void MainMenu_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
-            this.deskQuote = deskQuote;
-            this.mainMenu = mainMenu;
+            MainMenu mainMenu = (MainMenu)Tag;
+            mainMenu.Show();
+            this.Close();
         }
-
-        private void ViewAllQuotes_Load(object sender, EventArgs e)
-        {
-            string json = File.ReadAllText(@"quotes.json");
-            string text = File.ReadAllText(@"rushOrderPrices.txt");
-            labelQuotes.Text = json;
-            labelRushValue.Text = text;
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            MainMenu viewMainMenu = new MainMenu();
-            viewMainMenu.Tag = this;
-
-            this.Hide();
-        }
-
-        private void Button2_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
     }
 }
